6eb86ac45c7aabf41f9313739ef29b863ca2a50e
Ariel Ortenzi <arielortenzi@gmail.com>
2024-08-25 10:46:28 -0300

    fetch

--- script.js
+++ script.js
@@ -1,25 +1,3 @@
-// --------------------------- Array
-
-let listaProductos = [
-    { id: 1001, nombre: "Cinta transparente 48mm x 100mts", linea: "orpack", categoria: "cinta", precio: 1500, stock: 7, rutaImagen: "Cinta transparente 48mm x 100mtr.jpg" },
-    { id: 1002, nombre: "Cinta transparente 48mm x 1000mts", linea: "orpack", categoria: "cinta", precio: 1500, stock: 6, rutaImagen: "Cinta transparente 48mm x 1000mtr.jpg" },
-    { id: 1003, nombre: "Cinta transparente de 24mm x 50mtr", linea: "orpack", categoria: "cinta", precio: 1000, stock: 9, rutaImagen: "Cinta transparente de 24mm x 50mtr.jpg" },
-    { id: 1004, nombre: "Cinta transparente de 48mm x 50mtr", linea: "orpack", categoria: "cinta", precio: 1500, stock: 5, rutaImagen: "Cinta transparente de 48mm x 50mtr.jpg" },
-    { id: 2001, nombre: "Film adherente 38mm x 120mtr", linea: "ormai", categoria: "filmAdherente", precio: 7500, stock: 8, rutaImagen: "Film adherente 38 x 120mtrjpg.jpg" },
-    { id: 2002, nombre: "Film adherente 38mm x 300mtr", linea: "ormai", categoria: "filmAdherente", precio: 10300, stock: 45, rutaImagen: "Film adherente 38mm x 300mtr.jpg" },
-    { id: 3001, nombre: "Film adherente para alimentos 38cm x1000mtr", linea: "orpack", categoria: "filmAlimentos", precio: 7000, stock: 11, rutaImagen: "Film adherente para alimento 38cm x1000mtr.jpg" },
-    { id: 3002, nombre: "Film adherente para alimentos 45cm x 600mtr", linea: "orpack", categoria: "filmAlimentos", precio: 5000, stock: 16, rutaImagen: "Film adherente para alimento 45cm x 600mtr.jpg" },
-    { id: 3003, nombre: "Film adherente para alimentos 45cm x 1000mtr", linea: "orpack", categoria: "filmAlimentos", precio: 9700, stock: 31, rutaImagen: "Film adherente para alimento 45cm x 1000mtr.jpg" },
-    { id: 3004, nombre: "Film adherente para alimentos 38cm x 600mtr", linea: "orpack", categoria: "filmAlimentos", precio: 8100, stock: 27, rutaImagen: "Film adherente para alimentos 38cm x 600mtr.jpg" },
-    { id: 4001, nombre: "Film streetch 10cm x 800gr", linea: "orpack", categoria: "filmStreetch", precio: 6300, stock: 4, rutaImagen: "Film streetch 10cm x 800gr.jpg" },
-    { id: 4002, nombre: "Film Stretch 5cm x 500gr", linea: "orpack", categoria: "filmStreetch", precio: 4500, stock: 10, rutaImagen: "Film stretch 5cm x 500gr.jpg" },
-    { id: 4003, nombre: "Film Stretch de 10cm x500gr", linea: "orpack", categoria: "filmStreetch", precio: 4500, stock: 7, rutaImagen: "Film stretch de 10cm x500gr.jpg" },
-    { id: 4004, nombre: "Film Stretch industrial por 5k", linea: "orpack", categoria: "filmStreetch", precio: 21700, stock: 31, rutaImagen: "Film stretch industrial por 5k.jpg" },
-    { id: 4005, nombre: "Film stretch manguito 50cm x 2,5k", linea: "orpack", categoria: "filmStreetch", precio: 7500, stock: 3, rutaImagen: "Film stretch manguito 50cm x 2,5k.jpg" },
-    { id: 4006, nombre: "Film stretch negro industrial 5k", linea: "orpack", categoria: "filmStreetch", precio: 18400, stock: 34, rutaImagen: "Film stretch negro industrial 5k.jpg" },
-    { id: 9001, nombre: "Paletizadora Manual", linea: "orpack", categoria: "herramienta", precio: 45000, stock: 1, rutaImagen: "Paletizadora Manual.jpg" },
-]
-
 // --------------------------- Funcion principal
 
 function principal(productos) {
@@ -45,17 +23,25 @@ function principal(productos) {
     let botonComprar = document.getElementById("comprar");
     botonComprar.addEventListener("click", finalizarCompra);
 }
-let btnComprar = document.getElementById("comprar")
-btnComprar.disable = true;
 
-principal(listaProductos);
+//---------------------------- BD Productos
 
+async function consultarBD() {
+    try {
+        const response = await fetch("./listaProductos.json")
+        const productos = await response.json()
+        principal(productos)
+    } catch (error) {
+        sweetAlert("ALGO SALIÓ MAL!", "Se produjo un error al intentar cargar la lista de productos. Comuníquese con Soporte", "error")
+    }
+}
+consultarBD()
 
 // --------------------------- Funciones de storage
 function finalizarCompra() {
     let carrito = obtenerCarrito()
     if (carrito.length === 0){
-        sweetAlert("CARRITO VACÍO", "¡ Porfavor agregue productos al carrito !", "error")
+        sweetAlert("CARRITO VACÍO", "¡ Porfavor agregue productos al carrito !", "warning")
     } else{
         sweetAlert("GRACIAS POR SU COMPRA", "¡ Esperamos vuelva pronto !", "success")
         localStorage.removeItem("carrito");
@@ -218,8 +204,6 @@ function sumarUnidad(e, productos) {
     let carrito = obtenerCarrito()
     let disponibilidad = carrito.find(producto => producto.id === id)
     let prodOriginal = productos.find(producto => producto.id === id)
-    console.log(prodOriginal)
-    console.log(disponibilidad)
 
     if (prodOriginal.stock > 0){
         disponibilidad.unidades++;
@@ -251,6 +235,7 @@ function restarUnidad(e, productos) {
     }
 }
 
+// --------------------------- Funciones de alertas
 function tostada(text, className) {
     Toastify({
         text,
